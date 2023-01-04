d1da2a772ec68c42979521ab23b22a953275a496
changzhiwin <changzhiwin@gmail.com>
2023-01-04 10:57:38 +0800

    continue login

--- src/main/scala/ContinueLogin.scala
+++ src/main/scala/ContinueLogin.scala
@@ -1,7 +1,8 @@
 package zhiwin.spark.guide
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.Partitioner
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
 
 import org.apache.logging.log4j.scala.Logging
 
@@ -22,7 +23,26 @@ object ContinueLogin extends Logging {
       .load("./data/userlog.csv")
 
     df.printSchema()
-    df.show()
+
+    val windowSpec = Window.partitionBy("userId").orderBy("loginDate")
+      .rowsBetween(Window.unboundedPreceding, Window.currentRow)       // have orderBy, this the default
+
+    // 关键点：使用row_number()，取得相对值，让多行状态数据变成单行数据
+    df.withColumn("loginDate", to_date(col("loginTime")))
+      //.withColumn("pastDays", datediff(current_date(), col("loginDate")))
+      //.filter(col("pastDays").lt(8))
+      .filter(datediff(current_date(), col("loginDate")).lt(8))
+      .orderBy(col("userId"))
+      .withColumn("order", row_number().over(windowSpec))
+      .withColumn("diffDate", date_sub(col("loginDate"), col("order")))
+      .groupBy(col("userId"), col("diffDate"))
+      .agg(
+        count(col("userId")).as("countContinue"),  
+        min(col("loginDate")).as("earlyLogin"),
+        max(col("loginDate")).as("lastLogin")
+      )
+      .filter(col("countContinue").geq(3))
+      .show()
   }
 }
 
